94972738d545245306fda24e05b66b528d64280c
Tripta Sahni <Tripta.sahni@Geminisolutions.com>
2023-08-26 00:31:34 +0530

    Initial commit

---
+++ src/test/java/Testing.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class Testing {
+
+	@Test
+    public void startBrowser()
+    {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
+		driver.quit();
+}
+}
